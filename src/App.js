0f745815ec17c8ea57f6ec8d1018b2eb6b867501
KifilweJasmin <137528030+KifilweJasmin@users.noreply.github.com>
2024-01-03 21:29:20 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by KifilweJasmin and is open-sourced{" "}
-        <a
-          href="https://github.com/KifilweJasmin/react-weather-app"
-          target="blank"
-        >
-          on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by KifilweJasmin and is open-sourced{" "}
+          <a
+            href="https://github.com/KifilweJasmin/react-weather-app"
+            target="blank"
+          >
+            on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
