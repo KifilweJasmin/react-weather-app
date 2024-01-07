1e8887bcd2c6ec1ca25f4e581807a87eab05b13f
KifilweJasmin <137528030+KifilweJasmin@users.noreply.github.com>
2024-01-08 01:02:12 +0200

    Changed my apiUrl

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -27,7 +27,7 @@ export default function WeatherForecast(props) {
     let apiKey = "25d874c323417eb6fa36f60bca44be24";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
     return null;
